13f9c5ba12a3bbd9f0bd8e51e0ebcc82fe6e49b8
Ramesh <campustechlab@outlook.com>
2022-08-11 21:04:56 +0530

    challange

---
+++ nested objects.ps1
@@ -0,0 +1,21 @@
+﻿$Obj= ConvertFrom-Json '{ "A": "x", "B": {"C": "y"} }'
+# Or simply create the object:
+# $Obj= @{ A = "x"; B = @{C = "y"} }
+$Key = "B.C"
+$Value = "Some Value"
+$Obj.$Key = $Value
+
+
+# Functions
+function GetValue($object, $key)
+{
+    $p1,$p2 = $key.Split(".")
+    if($p2) { return GetValue -object $object.$p1 -key $p2 }
+    else { return $object.$p1 }
+}
+function SetValue($object, $key, $Value)
+{
+    $p1,$p2 = $key.Split(".")
+    if($p2) { SetValue -object $object.$p1 -key $p2 -Value $Value }
+    else { $object.$p1 = $Value }
+}
